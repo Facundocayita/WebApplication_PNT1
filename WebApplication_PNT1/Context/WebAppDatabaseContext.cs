e500616d3a595b99facfbbd3d12723bae75118ff
Facundo <facundocayita@hotmail.com>
2024-06-06 18:31:08 -0300

    Add project files.

---
+++ WebApplication_PNT1/Context/WebAppDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_PNT1.Models;
+using System.Collections.Generic;
+
+namespace WebApplication_PNT1.Context
+{
+    public class WebAppDatabaseContext : DbContext
+    {
+        public WebAppDatabaseContext(DbContextOptions<WebAppDatabaseContext> options): base(options)
+        {
+        }
+        public DbSet<Proyecto> Proyectos { get; set; }
+
+    }
+}
+
---
+++ WebApplication_PNT1/Models/Proyecto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebApplication_PNT1.Models
+{
+    public class Proyecto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdProyecto { get; set; }
+        public required string Descripcion { get; set; }
+        
+        public double Ancho { get; set; }
+        public double Alto {  get; set; }
+        public double Groso { get; set; }   
+
+        public DateTime FechaPedido { get; set; }
+        [EnumDataType(typeof(TipoProyecto))]
+        public TipoProyecto Tipo { get; set; }
+    }
+
+}
+
